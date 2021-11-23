85662fc0944e295151f61c38da8abfce1cb8108f
yurii-isaev <devhouse27@gmail.com>
2021-11-23 19:16:32 +0200

    feat(client): product filter

---
+++ client/src/components/catalog/catalog-item-select.vue
@@ -0,0 +1,81 @@
+<template>
+   <div class="catalog-item-select">
+
+      <p class="title" @click="isOptionsVisible = ! isOptionsVisible">
+         {{ selected }}
+      </p>
+
+      <div class="options" v-if="isOptionsVisible">
+         <p v-for="option in options"
+            :key="option.value"
+            @click="selectOption(option)">
+            {{ option.name }}
+         </p>
+      </div>
+
+   </div>
+</template>
+
+<script>
+  export default {
+     name: "catalog-item-select",
+     
+     props: {
+        options: {
+           type: Array, 
+           default() {
+              return []
+           }
+        }, 
+        selected: {
+           type: String, 
+           default: ''
+        }
+     }, 
+     
+     data() {
+        return {
+           isOptionsVisible: false
+        }
+     }, 
+     
+     methods: {
+        selectOption(option) {
+           this.$emit('selectOption', option);
+           this.isOptionsVisible = false;
+        },
+     }
+  }
+</script>
+
+<style scoped>
+  .catalog-item-select {
+     position: relative;
+     width: 200px;
+     cursor: pointer;
+     text-align: left;
+  }
+  .title {
+     border: solid 1px #2c3e50;
+     padding: 8px;
+  }
+
+  .catalog-item-select p {
+     margin: 0;
+  }
+  
+  .options {
+     border: solid 1px #2c3e50;
+     background: #e0e0e0;
+     position: absolute;
+     top: 30px;
+     left: 0;
+     width: 100%;
+     padding: 8px;
+  }
+
+  .options p:hover {
+     background: #e0e0e0;
+  }
+  
+</style>
--- client/src/components/catalog/catalog.vue
+++ client/src/components/catalog/catalog.vue
@@ -6,33 +6,58 @@
       
       <h1>Catalog</h1>
       
+      <catalog-item-select 
+         :selected="currentSelectedOption" 
+         :options="categories"
+         @selectOption="sortByCategories"
+      />
+      
       <div class="catalog__list">
          <catalog-item
-            v-for="product in PRODUCTS"
+            v-for="product in filtredProducts"
             :key="product.article"
             :product_data="product"
             @addToCart="addToCart"
          />
       </div>
+      
    </div>
 </template>
 
 <script>
-  import CatalogItem from '@/components/catalog/catalog-item'
-  import { mapActions, mapGetters } from 'vuex'
+  import { mapActions, mapGetters } from 'vuex';
+  import CatalogItem from '@/components/catalog/catalog-item';
+  import CatalogItemSelect from '@/components/catalog/catalog-item-select';
 
   export default {
      name: "v-catalog",
      
      components: {
-        CatalogItem
+        CatalogItem,
+        CatalogItemSelect
+     },
+     
+     data() {
+        return {
+           categories: [
+              {name: 'all', value: 'all'},
+              {name: 'snacks', value: 'snacks'},
+              {name: 'chips', value: 'chips'},
+           ],
+           currentSelectedOption: 'all',
+           sortedProducts: [],
+        }
      },
     
      computed: {
         ...mapGetters([
            'PRODUCTS',
            'CART'
-        ])
+        ]),
+
+        filtredProducts() {
+           return this.sortedProducts.length ? this.sortedProducts : this.PRODUCTS;
+        },
      },
      
      methods: {
@@ -40,21 +65,28 @@
            'GET_PRODUCTS_FROM_API',
            'ADD_TO_CART'
         ]),
+        
         addToCart(data) {
-           this.ADD_TO_CART(data)
+           this.ADD_TO_CART(data);
         },
-     },
-     
-     // Load data check
-     mounted() {
-        this.GET_PRODUCTS_FROM_API().then((res) => {
-           if (res.data) {
-              console.log("Load data successuly")
+        
+        sortByCategories(category) {
+           this.sortedProducts = this.PRODUCTS.filter(product => product.category === category.name);
+           this.currentSelectedOption = category.name;
+        },
+
+        async loadDataFromApi() {
+           try {
+              await this.GET_PRODUCTS_FROM_API();
+           } catch (error) {
+              console.error(error);
+              return error;
            }
-        }).catch((error) => {
-           console.log(error)
-           return error
-        })
+        }
+     },
+
+     created() {
+        this.loadDataFromApi();
      }
   }
 </script>
@@ -64,7 +96,7 @@
      &__list {
         display: flex;
         flex-wrap: wrap;
-        justify-content: space-between;
+        justify-content: space-evenly;
         align-items: center;
      }
      &__link_to_cart {
