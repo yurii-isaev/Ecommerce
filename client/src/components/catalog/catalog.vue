5aeb85bf7e61de49ba7e849f33b75983e8d2c329
yurii-isaev <devhouse27@gmail.com>
2021-11-26 18:20:36 +0200

    feat(client): product filters range slider

--- client/src/components/catalog/catalog.vue
+++ client/src/components/catalog/catalog.vue
@@ -1,17 +1,38 @@
 <template>
    <div class="catalog">
       <router-link :to="{ name: 'cart' }">
-      <div class="catalog__link_to_cart">Cart: {{ CART.length }}</div>
+         <div class="catalog__link-to-cart">Cart: {{ CART.length }}</div>
       </router-link>
-      
+
       <h1>Catalog</h1>
-      
-      <catalog-item-select 
-         :selected="currentSelectedOption" 
-         :options="categories"
-         @selectOption="sortByCategories"
-      />
-      
+
+      <div class="catalog__filters">
+         <catalog-item-select
+            :selected="currentSelectedOption"
+            :options="categories"
+            @selectOption="sortByCategories"
+         />
+         <div class="catalog__filters-range-slider">
+            <input
+               type="range" min="0" max="1000"
+               step="10"
+               v-model.number="minPrice"
+               @change="setRangeSlider"
+            >
+            <input
+               type="range" min="0" max="1000"
+               step="10"
+               v-model.number="maxPrice" 
+               @change="setRangeSlider"
+            >
+         </div>
+         
+         <div class="catalog__filters-range-values">
+            <p>Min: {{ minPrice }}</p>
+            <p>Max: {{ maxPrice }}</p>
+         </div>
+      </div>
+
       <div class="catalog__list">
          <catalog-item
             v-for="product in filtredProducts"
@@ -20,7 +41,6 @@
             @addToCart="addToCart"
          />
       </div>
-      
    </div>
 </template>
 
@@ -30,13 +50,13 @@
   import CatalogItemSelect from '@/components/catalog/catalog-item-select';
 
   export default {
-     name: "v-catalog",
-     
+     name: "component-catalog",
+
      components: {
         CatalogItem,
         CatalogItemSelect
      },
-     
+
      data() {
         return {
            categories: [
@@ -46,9 +66,11 @@
            ],
            currentSelectedOption: 'all',
            sortedProducts: [],
+           minPrice: 0,
+           maxPrice: 1000
         }
      },
-    
+
      computed: {
         ...mapGetters([
            'PRODUCTS',
@@ -59,32 +81,49 @@
            return this.sortedProducts.length ? this.sortedProducts : this.PRODUCTS;
         },
      },
-     
+
      methods: {
         ...mapActions([
-           'GET_PRODUCTS_FROM_API',
+           'GET_PRODUCTS_FROM_API', 
            'ADD_TO_CART'
-        ]),
+        ]), 
         
         addToCart(data) {
            this.ADD_TO_CART(data);
         },
-        
-        sortByCategories(category) {
-           this.sortedProducts = this.PRODUCTS.filter(product => product.category === category.name);
-           this.currentSelectedOption = category.name;
-        },
 
+        sortByCategories(selectedOption) {
+           const { minPrice, maxPrice, PRODUCTS } = this;
+           this.sortedProducts = PRODUCTS.filter(product => product.price >= minPrice && product.price <= maxPrice);
+
+           if (selectedOption) {
+              this.sortedProducts = this.sortedProducts.filter(product => {
+                 this.selected = selectedOption.name;
+                 return product.category === selectedOption.name;
+              });
+           }
+        },
+        
+        setRangeSlider() {
+           if (this.minPrice > this.maxPrice) {
+              let temp = this.maxPrice;
+              this.maxPrice = this.minPrice;
+              this.minPrice = temp;
+           }
+           this.sortByCategories();
+        }, 
+        
         async loadDataFromApi() {
            try {
               await this.GET_PRODUCTS_FROM_API();
+              this.sortByCategories();
            } catch (error) {
               console.error(error);
               return error;
            }
         }
-     },
-
+     }, 
+     
      created() {
         this.loadDataFromApi();
      }
@@ -92,19 +131,73 @@
 </script>
 
 <style lang="scss" scoped>
-  .catalog {
+
+  $padding: 10px;  
+  
+  .catalog { 
      &__list {
         display: flex;
         flex-wrap: wrap;
         justify-content: space-evenly;
         align-items: center;
-     }
-     &__link_to_cart {
-        position: absolute;
+     } 
+     
+     &__link-to-cart {
+        position: fixed;
         top: 10px;
         right: 10px;
-        padding: $padding*2;
-        border: solid 1px;
-     }
+        padding: $padding * 2;
+        border: 1px solid #aeaeae;
+        background: #ffffff;
+     } 
+     
+     &__filters { 
+        &-range-slider { 
+           input[type=range] {
+              position: absolute;
+              left: 65%;
+              top: 180px;
+           } 
+        } 
+        
+        &-range-values { 
+           p {
+              margin: 5px 0;
+           } 
+        } 
+     } 
+  }  
+  
+  input[type=range] {
+     -webkit-appearance: none; 
+     
+     &::-webkit-slider-runnable-track {
+        width: 300px;
+        height: 5px;
+        background: #ddd;
+        border: none;
+        border-radius: 3px;
+     } 
+     
+     &::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        border: 1.5px solid #C1C1C1;
+        height: 16px;
+        width: 16px;
+        border-radius: 50%;
+        background: #EDEDED;
+     } 
+     
+     &:focus {
+        outline: none;
+     } 
+     
+     &::-webkit-slider-runnable-track {
+        width: 100%;
+        height: 5px;
+        box-shadow: 1px 1px 1px #C6C6C6, 0 0 1px #787878;
+        border-radius: 2px;
+        border: 0.2px solid #787878;
+     } 
   }
 </style>
